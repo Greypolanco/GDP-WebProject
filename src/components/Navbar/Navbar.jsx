d3785c32db40ec373df661dcf10ff6ad629f5095
Levid <jassontejada@gmail.com>
2024-04-01 17:08:41 -0400

    Colocando Logout provisional

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Collapse } from "react-bootstrap";
 import { useAuth } from "../../context/AppContext";
-
+import Logout from '../Logout/Logout';
 export const Navbar = () => {
   const [open, setOpen] = useState(false);
   const userLogged = useAuth();
@@ -38,6 +38,11 @@ export const Navbar = () => {
                 <Link className="nav-link" aria-current="page" to="/tasks">
                   Tareas
                 </Link>
+              </li>
+              <li>
+                <div className="nav-link" aria-current="page">
+                  <Logout/>
+                </div>
               </li>
             </ul>
           </div>
