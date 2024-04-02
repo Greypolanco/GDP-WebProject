7bf3e4099fc72cd467092f5e8ca457e33ab85679
Levid <jassontejada@gmail.com>
2024-04-02 00:04:39 -0400

    Agregando boton Logout y Login al navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,51 +2,55 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Collapse } from "react-bootstrap";
 import { useAuth } from "../../context/AppContext";
-import Logout from '../Logout/Logout';
+import LogoutButton from '../Logout/LogoutButton';
+import LoginButton from "../Login/LoginButton";
+
 export const Navbar = () => {
   const [open, setOpen] = useState(false);
-  const userLogged = useAuth();
+  const { user } = useAuth();
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="#">
           @SingletonCoders
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          onClick={() => setOpen(!open)}
-          aria-expanded={open}
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        <div className="d-flex align-items-center">
+          <button
+            className="navbar-toggler"
+            type="button"
+            onClick={() => setOpen(!open)}
+            aria-expanded={open}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
         <Collapse in={open}>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
+            <ul className="navbar-nav mr-auto">
               <li className="nav-item">
                 <Link className="nav-link" aria-current="page" to="/">
                   Inicio
                 </Link>
               </li>
-              <li>
+              <li className="nav-item">
                 <Link className="nav-link" aria-current="page" to="/projects">
                   Proyectos
                 </Link>
               </li>
-              <li>
+              <li className="nav-item">
                 <Link className="nav-link" aria-current="page" to="/tasks">
                   Tareas
                 </Link>
               </li>
-              <li>
-                <div className="nav-link" aria-current="page">
-                  <Logout/>
-                </div>
-              </li>
             </ul>
           </div>
         </Collapse>
+        <div className="justify-content-end">
+          <div className="navbar-nav">
+            {user ? <LogoutButton /> : <LoginButton />}
+          </div>
+        </div>
       </div>
     </nav>
   );
