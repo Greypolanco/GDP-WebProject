1635289d19b1e3fe67732c6818c74df592288612
Levid <jassontejada@gmail.com>
2024-04-04 10:30:26 -0400

    Reformatendo ProjectItem

--- src/components/Projects/ProjectItem.jsx
+++ src/components/Projects/ProjectItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './ProjectItem.css';
-import { getStatusColor, formatDate } from '../../utils/utils';
+import { getStatusColor, formatDate, getStatusText } from '../../utils/utils';
 import '../../utils/utils.css';
 
 export const ProjectItem = ({ project, onSelect }) => {
@@ -10,25 +10,20 @@ export const ProjectItem = ({ project, onSelect }) => {
   }
 
   return (
-    <div className='card mb-3'>
-      <div className='card-header'>
-        <h3>{project.title}</h3>
+    <div className='item-container card m-2'>
+      <div className='img-container'>
+        <img src='https://placehold.co/200x200' width={200} height={200} alt={project.title} />
       </div>
-      <div className='card-body'>
-        <div className='row'>
-          <div className='col-md-3'>
-            <img src='https://placehold.co/200x200' alt={project.title} className='' />
-          </div>
-          <div className='col-12 col-md-6'>
-            <p><strong>Descripción:</strong> {project.description}</p>
-            <p><strong>Fecha de creación:</strong> {formatDate(project.created_at)}</p>
-            <p><strong>Estado:</strong> <span className={`badge ${getStatusColor(project.status)}`}>{project.status}</span></p>
-          </div>
-        </div>
-        <div className='card-footer'>
-          <button className='btn btn-outline-primary bi bi-eye m-1' onClick={handleClick}></button>
-        </div>
-      </div>
+      <section className='info-container p-2'>
+        <h5>{project.title}</h5>
+        <p><strong>Descripción:</strong> {project.description}</p>
+        <p><strong>Fecha de inicio:</strong> {formatDate(project.startDate)}</p>
+        <p className='d-flex'>
+          <strong>Estado:</strong>
+          <div className={`ms-1 mt-1 me-1 ${getStatusColor(project.status)}`}>
+          </div>{getStatusText(project.status)}
+        </p>
+      </section>
     </div>
   );
 };
