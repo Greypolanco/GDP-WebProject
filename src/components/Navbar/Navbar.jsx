0842d469d10e0dd05c2d72d7eedc1c08fd67a432
Levid <jassontejada@gmail.com>
2024-04-04 01:19:05 -0400

    Actualizando Navbar con opciones desplegables

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import { Collapse } from "react-bootstrap";
 import { useAuth } from "../../context/AppContext";
@@ -7,20 +7,38 @@ import LoginButton from "../Login/LoginButton";
 
 export const Navbar = () => {
   const [open, setOpen] = useState(false);
+  const [pjSubmenu, setPjSubmenu] = useState(false);
   const { user, setUser } = useAuth();
   const userLogged = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
+  const dropdownRef = useRef(null);
+
+  const handleSubmenuClick = () => {
+    setPjSubmenu(!pjSubmenu);
+  }
 
   useEffect(() => {
     if (userLogged) {
       setUser(userLogged);
     }
-  },[]);
+
+    const handleOutsideClick = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setPjSubmenu(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
 
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="#">
-          @SingletonCoders
+        <Link className="navbar-brand important" to="#">
+          {user ? `${user.name} ${user.surname}` : '@SingletonCoders'}
         </Link>
         <div className="d-flex align-items-center">
           <button
@@ -40,10 +58,20 @@ export const Navbar = () => {
                   Inicio
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/projects">
+              <li className="nav-item dropdown" ref={dropdownRef}>
+                <Link
+                  className="nav-link dropdown-toggle"
+                  to="#"
+                  id="navbarDropdown"
+                  role="button"
+                  onClick={handleSubmenuClick}
+                >
                   Proyectos
                 </Link>
+                <div className={`dropdown-menu ${pjSubmenu ? 'show' : ''}`} aria-labelledby="navbarDropdown">
+                  <Link className="bi bi-bag-plus dropdown-item" to="/projects" onClick={handleSubmenuClick}> Lista</Link>
+                  <Link className="bi bi-list-stars dropdown-item" to="/proyectos/consultas" onClick={handleSubmenuClick}> Consultas</Link>
+                </div>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" aria-current="page" to="/tasks">
