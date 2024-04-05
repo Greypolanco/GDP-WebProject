0f85709c6f550d6946829e0de918c336ee85e82f
Levid <jassontejada@gmail.com>
2024-04-05 11:51:03 -0400

    Switch Light-Dark theme

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
@@ -7,18 +8,41 @@ import { AuthProvider } from './context/AppContext.jsx'
 import { Navbar, ProjectsIndex, Login, ProjectConsult, ProjectView } from "./components/index.jsx";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(prevMode => !prevMode);
+  };
+
+  useEffect(() => {
+    const localStorageMode = localStorage.getItem('darkMode');
+    if (localStorageMode) {
+      setIsDarkMode(localStorageMode === 'true');
+    } else {
+      setIsDarkMode(window.matchMedia('(prefers-color-scheme: dark)').matches);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('darkMode', isDarkMode);
+    document.body.classList.toggle('dark-mode', isDarkMode);
+  }, [isDarkMode]);
+
   return (
     <AuthProvider>
       <BrowserRouter>
         <Navbar />
         <div className="container my-4 p-3">
+          <button
+            className={`btn btn-${isDarkMode ? 'dark' : 'light'} bi bi-${isDarkMode ? 'sun' : 'moon'}`}
+            onClick={toggleDarkMode}>
+          </button>
           <Routes>
             <Route exact path="/login" element={<Login />} />
-
             {/* Project routes */}
             <Route exact path="/projects" element={<ProjectsIndex />} />
             <Route exact path="/projects/:id" element={<ProjectView />} />
-            <Route path="/projects/consult" element={<ProjectConsult/>}/>
+            <Route path="/projects/consult" element={<ProjectConsult />} />
           </Routes>
         </div>
       </BrowserRouter>
