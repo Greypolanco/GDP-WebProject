97b01c5ca1506647ef3a24c9fbbaddce09a22de5
Greypolanco <greilyntejada03@gmail.com>
2024-04-05 19:25:39 -0400

    Renderizando los componentes de task

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import "./App.css";
 import { AuthProvider } from './context/AppContext.jsx'
 //Componentes
-import { Navbar, ProjectsIndex, Login, ProjectConsult, ProjectView } from "./components/index.jsx";
+import { Navbar, ProjectsIndex, Login, ProjectConsult, ProjectView, TaskIndex, TaskView } from "./components/index.jsx";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -43,6 +43,8 @@ function App() {
             <Route exact path="/projects" element={<ProjectsIndex />} />
             <Route exact path="/projects/:id" element={<ProjectView />} />
             <Route path="/projects/consult" element={<ProjectConsult />} />
+            <Route path='/tasks' element={<TaskIndex />} />
+            <Route path='/tasks/:id' element={<TaskView />} />
           </Routes>
         </div>
       </BrowserRouter>
