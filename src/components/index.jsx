68e2f7fc24ccee2152386f5fcf020f5cfd0a0ba4
Levid <jassontejada@gmail.com>
2024-04-05 22:57:15 -0400

    Estableciendo ruta del formulario

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,6 +2,7 @@ export { default as Navbar } from "./Navbar/Navbar";
 export { default as ProjectsIndex } from "./Projects/ProjectsIndex";
 export { default as ProjectConsult } from "./Projects/ProjectConsult";
 export { default as ProjectView } from "./Projects/ProjectView";
+export { default as ProjectForm } from "./Projects/ProjectForm";
 export { default as Login } from "./Login/Login";
 export {default as TaskIndex} from "./Tasks/TaskIndex";
 export {default as TaskView} from "./Tasks/TaskView";
