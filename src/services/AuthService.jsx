914ecc6564f48820a109189da927091e861fe8d3
Greypolanco <greilyntejada03@gmail.com>
2024-04-14 19:01:21 -0400

    Agregando servicio de register.

--- src/services/AuthService.jsx
+++ src/services/AuthService.jsx
@@ -1,5 +1,9 @@
-import { Login } from "../remote/WebApi";
+import { Login, Register } from "../remote/WebApi";
 
 export const login = async (credential) => {
     return await Login(credential);
+}
+
+export const register = async (user) => {
+    return await Register(user);
 }
